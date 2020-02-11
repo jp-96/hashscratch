0516682455cbd5645dd34548dc732e77d8329231
jp-96 <h.kuroyanagi.css@gmail.com>
2020-02-11 17:33:38 +0900

    limited

---
+++ background.js
@@ -0,0 +1,16 @@
+﻿// #S - Hash Scratch
+// Copyright (c) 2020 ULTIMATE PICTURES. All rights reserved.
+// https://ultimate.pictures/
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  const urlScratch = "https://scratch.mit.edu/projects/";
+  const urlChampierre = "https://champierre.github.io/scratch3/";
+  if ((tab.url.slice(0,urlScratch.length) == urlScratch) || (tab.url.slice(0,urlChampierre.length) == urlChampierre)){
+    chrome.tabs.sendMessage(tab.id, {command: "onClicked"}, function(response) {
+      console.log(response);
+    });
+  } else {
+    console.log('Not supported: ' + tab.url);
+  }
+})
