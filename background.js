5a09ff362e0a4ba787a0a7372242a48425020bd8
jp-96 <h.kuroyanagi.css@gmail.com>
2020-02-12 10:17:10 +0900

    tab.url

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ var isBussy=false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   const urlScratch = "https://scratch.mit.edu/projects/";
   const urlChampierre = "https://champierre.github.io/scratch3/";
-  if ((tab.url.slice(0,urlScratch.length) == urlScratch) || (tab.url.slice(0,urlChampierre.length) == urlChampierre)){
+  if (('url' in tab) && ((tab.url.slice(0,urlScratch.length) == urlScratch) || (tab.url.slice(0,urlChampierre.length) == urlChampierre))){
     if (isBussy){return;}
     isBussy = true;
     chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 100] });
