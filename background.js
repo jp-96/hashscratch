97cf9641f1dd90a43c47b0d8f80012f113275356
jp-96 <h.kuroyanagi.css@gmail.com>
2020-04-04 20:13:10 +0900

    support mBlock

--- background.js
+++ background.js
@@ -8,8 +8,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   const urlScratch = "https://scratch.mit.edu/projects/";
   const urlChampierre = "https://champierre.github.io/scratch3/";
   const urlML4kids = "https://machinelearningforkids.co.uk/";
+  const urlmBlock = "https://ide.mblock.cc/";
   const urlMicrobit = "https://makecode.microbit.org/"
-  if (('url' in tab) && ((tab.url.slice(0,urlScratch.length) == urlScratch) || (tab.url.slice(0,urlChampierre.length) == urlChampierre) || (tab.url.slice(0,urlML4kids.length) == urlML4kids) || (tab.url.slice(0,urlMicrobit.length) == urlMicrobit))){
+  if (('url' in tab) && ((tab.url.slice(0,urlScratch.length) == urlScratch) || (tab.url.slice(0,urlChampierre.length) == urlChampierre) || (tab.url.slice(0,urlML4kids.length) == urlML4kids) || (tab.url.slice(0,urlmBlock.length) == urlmBlock) || (tab.url.slice(0,urlMicrobit.length) == urlMicrobit))){
     if (isBussy){return;}
     isBussy = true;
     chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 100] });
